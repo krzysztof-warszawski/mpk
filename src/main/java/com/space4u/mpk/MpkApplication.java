cdbc0de4e7bc6dbf42eac72186bb992572961cf4
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-02-13 13:00:55 +0100

    initial

---
+++ src/main/java/com/space4u/mpk/MpkApplication.java
@@ -0,0 +1,13 @@
+package com.space4u.mpk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/space4u/mpk/MpkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space4u.mpk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
