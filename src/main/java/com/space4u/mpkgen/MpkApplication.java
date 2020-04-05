ed894a5e105028a458f3c9351969abc894fd7384
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-04-05 13:09:46 +0200

    update of gitignore

--- src/main/java/com/space4u/mpkgen/MpkApplication.java
+++ src/main/java/com/space4u/mpkgen/MpkApplication.java
@@ -4,12 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
-/*
-* ADMIN
-* user: susan
-* pass: fun123
-*/
 public class MpkApplication {
 
 	public static void main(String[] args) {
